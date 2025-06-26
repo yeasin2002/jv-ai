8765e6e6f3f44ba47541d7b97778b88da08eb018
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-06-27 04:16:16 +0600

    refactor: redux slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
-import counterReducer from "./features/counter/counterSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     auth: authReducer,
   },
 });
