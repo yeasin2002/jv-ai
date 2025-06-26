d505732a7d3ce7b4dd0669b6f9fe5db4d601e990
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-06-27 03:21:32 +0600

    chore: setup redux and  react

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 import { Home } from "@/page/home";
 import { NotFound, RootErrorBoundary } from "./page";
+import { store } from "./store";
 
 // Create a client
 const queryClient = new QueryClient();
@@ -12,10 +14,12 @@ const App = () => {
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <Routes>
-          <Route path="/" element={<Home />} errorElement={<RootErrorBoundary />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Provider store={store}>
+          <Routes>
+            <Route path="/" element={<Home />} errorElement={<RootErrorBoundary />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Provider>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </BrowserRouter>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,8 @@
-// redux or zustand store
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./features/counter/counterSlice";
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
